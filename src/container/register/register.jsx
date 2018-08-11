56493d9af7af4037adc0e446f076b856781c7bbc
zxfzxyrxf <zhangxiaofei@atguigu.com>
2018-08-11 12:29:28 +0800

    router

---
+++ src/container/register/register.jsx
@@ -0,0 +1,10 @@
+import React, {Component} from 'react'
+/*
+注册路由组件
+ */
+export default class Register extends Component {
+
+  render () {
+    return <div>Register</div>
+  }
+}
--- src/index.js
+++ src/index.js
@@ -3,10 +3,33 @@
  */
 import React from 'react'
 import ReactDOM from 'react-dom'
+import {HashRouter, Switch, Route} from 'react-router-dom'
+import Login from './container/login/login'
+import Register from './container/register/register'
+import Main from './container/main/main'
 
-import App from './App'
 
 // 渲染组件标签到页面
-ReactDOM.render(<App/>, document.getElementById('root'))
+ReactDOM.render((
+  <HashRouter>
+    <Switch>
+      <Route path='/login' component={Login}/>
+      <Route path='/register' component={Register}/>
+      <Route component={Main}/>
+    </Switch>
+  </HashRouter>
+), document.getElementById('root'))
+
+
+/*
+路由是什么?
+  就是一个key:value的映射关系
+路由的分类?
+  1. 后台路由: path---callback
+  2. 前台路由: path---component
+作用?
+  后台路由: 当服务器接收到请求时, 根据请求的path找到对应的路由, 由路由的回调函数来处理请求, 返回响应
+  前台路由: 当请求某个路由地址时, 根据请求的path找到对应的路由, 显示路由对应的组件界面
+ */
 
 
