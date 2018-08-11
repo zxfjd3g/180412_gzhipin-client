d7f63b07d0baee500710b0afdb7302c51757e5ee
zxfzxyrxf <zhangxiaofei@atguigu.com>
2018-08-11 10:57:54 +0800

    init

---
+++ src/App.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import {Button} from 'antd-mobile'
+
+export default function App() {
+  return <Button type='primary'>Hello react!</Button>
+}
---
+++ src/index.js
@@ -0,0 +1,12 @@
+/*
+入口JS
+ */
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import App from './App'
+
+// 渲染组件标签到页面
+ReactDOM.render(<App/>, document.getElementById('root'))
+
+
