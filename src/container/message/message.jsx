fbc56d97baa61ff883c0029126a076cc10e8860a
zxfzxyrxf <zhangxiaofei@atguigu.com>
2018-08-17 16:40:45 +0800

    unReadCount

--- src/container/message/message.jsx
+++ src/container/message/message.jsx
@@ -12,25 +12,42 @@ class Message extends Component {
   /*
   从chatMsgs中找出每个聊天中的最后一个msg组成的数组
   1. 创建一个对象容器(lastMsgObjs): 用来存储每个分组的lastMsg, key是chat_id, value是msg
-  2. 遍历chatMsgs, 取出msg, 并判断msg是否是所属分组最后一条, 如果是保存到chatMsgObjs中
+  2. 遍历chatMsgs, 取出msg, 并判断msg是否是所属分组最后一条, 如果是保存到lastMsgObjs中
   3. 得到lastMsgObjs的所有value组成的数组(lastMsgs)
   4. 对lastMsgs数组进行排序
    */
-  getLastMsgs = (chatMsgs) => {
+  getLastMsgs = (chatMsgs, meId) => {
     // 1. 创建一个对象容器(lastMsgObjs): 用来存储每个分组的lastMsg, key是chat_id, value是msg
     const lastMsgObjs = {}
 
     // 2. 遍历chatMsgs, 取出msg, 并判断msg是否是所属分组最后一条, 如果是保存到lastMsgObjs中
     chatMsgs.forEach(msg => {
+
+      // 未读消息: !msg.read && msg.to===meId
+      // 对当前msg进行统计
+      if(!msg.read && msg.to===meId) {
+        msg.unReadCount = 1
+      } else {
+        msg.unReadCount = 0
+      }
+
       const chatId = msg.chat_id
       // 获取当前组的lastMsg
       const lastMsg = lastMsgObjs[chatId]
       if(!lastMsg) {// 当前msg就是所属组的lastMsg
         lastMsgObjs[chatId] = msg
+
       } else {// 有同组2条msg, 只有当当前msg更晚, 保存当前msg
+
+        // 在确定lastMsg之前: 统计新的unReadCount
+        const unReadCount = msg.unReadCount + lastMsg.unReadCount
+
         if(msg.create_time>lastMsg.create_time) {
           lastMsgObjs[chatId] = msg
         }
+
+        // 已经确定了lastMsg: 给当前组lastMsg指定unReadCount
+        lastMsgObjs[chatId].unReadCount = unReadCount
       }
     })
 
@@ -42,32 +59,40 @@ class Message extends Component {
       return msg2.create_time - msg1.create_time  // 降序
       // return msg1.create_time - msg2.create_time // 升序
     })
+
+    return lastMsgs
   }
 
   render() {
     const {user} = this.props
+    const meId = user._id
     const {users, chatMsgs} = this.props.chat
 
-    const lastMsgs = this.getLastMsgs(chatMsgs)
+    const lastMsgs = this.getLastMsgs(chatMsgs, meId)
 
     return (
       <List style={{marginTop: 50, marginBottom: 50}}>
-        <Item
-          extra={<Badge text={3}/>}
-          thumb={require(`../../assets/images/头像1.png`)}
-          arrow='horizontal'
-        >
-          你好
-          <Brief>nr1</Brief>
-        </Item>
-        <Item
-          extra={<Badge text={0}/>}
-          thumb={require(`../../assets/images/头像2.png`)}
-          arrow='horizontal'
-        >
-          你好2
-          <Brief>nr2</Brief>
-        </Item>
+        {
+          lastMsgs.map(msg => {
+            // 得到目标用户的id
+            const targetId =  meId===msg.from ? msg.to : msg.from
+            // 得到目标用户的信息
+            const targetUser = users[targetId]
+
+            return (
+              <Item
+                key={msg._id}
+                extra={<Badge text={msg.unReadCount}/>}
+                thumb={require(`../../assets/images/${targetUser.header}.png`)}
+                arrow='horizontal'
+                onClick={() => this.props.history.push(`/chat/${targetId}`)}
+              >
+                {msg.content}
+                <Brief>{targetUser.username}</Brief>
+              </Item>
+            )
+          })
+        }
       </List>
     )
   }
